e7be20d2e298564741942738248c160450eb0b42
rudrapratapsingh11018 <rudrapratapsingh.cse@gmail.com>
2026-07-18 23:29:12 +0530

    Added 266A stones on the table for 800 level

---
+++ 01_Arrays/demo.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // preprocessor directives 
+#include <stdio.h> 
+using namespace std ;
+int main() {
+    cout<< " so called 1st program "<< endl;
+    cout<< " my name is R " << endl;
+return 0;
+}
