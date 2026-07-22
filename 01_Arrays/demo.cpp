322815323e0cb2bb438672b16313be4f1139dd64
rudrapratapsingh11018 <rudrapratapsingh.cse@gmail.com>
2026-07-22 12:23:13 +0530

    Added 112 A Petya and Strings 800 level

--- 01_Arrays/demo.cpp
+++ 01_Arrays/demo.cpp
@@ -1,8 +1,9 @@
-#include <iostream> // preprocessor directives 
-#include <stdio.h> 
-using namespace std ;
-int main() {
-    cout<< " so called 1st program "<< endl;
-    cout<< " my name is R " << endl;
-return 0;
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+   int marks [5] = {90, 80, 70, 60 ,50};
+ cout<<marks[5];
+    return 0;
 }
